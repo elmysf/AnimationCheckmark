77a68a0a3ee3a328e47f5fcfd8b7affa5c358290
elmysf <elmysufiandy09@gmail.com>
2021-10-26 10:55:57 +0700

    Initial Commit

---
+++ Checkmark jatiper/Checkmark_jatiperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Checkmark_jatiperApp.swift
+//  Checkmark jatiper
+//
+//  Created by Sufiandy Elmy on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Checkmark_jatiperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
